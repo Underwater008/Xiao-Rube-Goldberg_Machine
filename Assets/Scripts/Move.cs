6833265b9a89d57614692bb8446e5c18dddaaf42
zhixuan xiao <1@xiao.studio>
2021-09-21 00:19:14 -0400

    Press to start
    working

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
     }
 }
